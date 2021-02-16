a2240545eb4de3e5ff4a5650f4d959244bc0c78a
Emmett <theemmettwhite@gmail.com>
2021-02-16 13:15:11 +0100

    kick command

--- bot/app.js
+++ bot/app.js
@@ -11,7 +11,8 @@ const
     config = require('../config/config.json'),
     callbot = require('./modules/callbot.js'),
     help = require('./modules/help.js'),
-    avatar = require('./modules/avatar.js');
+    avatar = require('./modules/avatar.js'),
+    kick = require('./modules/kick.js');
 
 /**
  * Functions
@@ -22,6 +23,7 @@ client.on('message', async (msg) => {
     callbot.show(msg);
     help.show(msg);
     avatar.show(msg);
+    kick.show(msg);
 });
 
 client.login(config.token);
---
+++ bot/modules/kick.js
@@ -0,0 +1,37 @@
+const
+    {Client} = require('discord.js'),
+    client = new Client(),
+
+    config = require('../../config/config.json');
+
+exports.show = async (msg) => {
+    if (msg.content.startsWith(`${config.prefix}kick`)) {
+        if (!msg.member.roles.cache.some(role => role.name === 'ADMIN')) {
+            return msg.channel.send('You don\'t have a permission to kick members!');
+        }
+
+        const
+            user = msg.mentions.users.first(),
+            args = msg.content.slice(config.prefix.length).trim().split(' ');
+
+        if (user) {
+            const member = msg.guild.members.resolve(user);
+
+            if (member) {
+                member
+                    .kick(`${user.tag} has been kicked from the server! Reason: ${args[2]}`)
+                    .then(() => {
+                        msg.channel.send(`${user.tag} has been kicked from the server! Reason: ${args[2]}`)
+                    })
+                    .catch((err) => {
+                        msg.channel.send('I can\'t kick that member...');
+                        console.log(err);
+                    });
+            } else {
+                msg.channel.send('That user isn\'t in this guild...');
+            }
+        } else {
+            msg.channel.send('You didn\'t mention the user to kick...');
+        }
+    }
+}
